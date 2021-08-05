71df0557ec48b40c02b50ef79953b5823078dd4d
MydroX <maxime@zinutti.fr>
2021-08-05 05:54:20 +0200

    Add waving movements of boids

--- internal/boids/boids.go
+++ internal/boids/boids.go
@@ -13,21 +13,27 @@ import (
 )
 
 type Boid struct {
-	Body            *imdraw.IMDraw
-	MovingDirection *tools.Vector
-	Width           float64
-	Height          float64
-	Origin          *tools.Coordinates
-	Vertex          *tools.Coordinates
-	Angle           float64 // Angle are saved in radians
-	Speed           uint16
+	Body                 *imdraw.IMDraw
+	Width                float64
+	Height               float64
+	Origin               *tools.Coordinates
+	Vertex               *tools.Coordinates
+	Speed                uint16
+	AngleSpeed           float64 // Angle speed is saved in radians
+	Angle                float64 // Angle is saved in radians
+	TravelAngle          float64
+	TravelAngleDirection bool // true is left, false is right
+
+	MaxTravelAngleValue uint16
+	MaxTravelAngle      float64
 }
 
 func (b *Boid) construct(originX float64, originY float64, angle float64) {
 	b.Width = 16
 	b.Height = 20
-	b.Speed = 200
-
+	b.Speed = 100 // Defautl 180
+	b.AngleSpeed = 20 * math.Pi / 180
+	b.MaxTravelAngleValue = 13
 	b.Origin = &tools.Coordinates{}
 	b.Origin.X = originX
 	b.Origin.Y = originY
@@ -37,20 +43,23 @@ func (b *Boid) construct(originX float64, originY float64, angle float64) {
 	b.Body.Color = pixel.RGB(0, 0, 0)
 
 	//Find vertex
-	b.Vertex = trig.FindPointFromPoint(b.Origin, b.Angle, b.Height)
+	b.Vertex = trig.FindPointFromPoint(b.Origin, b.TravelAngle, b.Height)
 }
 
-func New(x float64, y float64, angle float64) Boid {
+func New(x float64, y float64, angle float64, travelAngleDirection bool) Boid {
 	b := Boid{}
 
 	// Construct
 	b.construct(x, y, angle)
 
-	//Initial movement
-	b.MovingDirection = &tools.Vector{
-		X: 100,
-		Y: 0,
+	if travelAngleDirection == true {
+		b.TravelAngleDirection = travelAngleDirection
+		b.MaxTravelAngle = b.Angle + float64(b.MaxTravelAngleValue)*math.Pi/180
+	} else if travelAngleDirection == false {
+		b.TravelAngleDirection = travelAngleDirection
+		b.MaxTravelAngle = b.Angle - float64(b.MaxTravelAngleValue)*math.Pi/180
 	}
+	b.TravelAngle = b.Angle
 
 	//Position
 	b.drawBoidBody()
@@ -58,8 +67,8 @@ func New(x float64, y float64, angle float64) Boid {
 }
 
 func (b *Boid) GetSidePoints() (leftPoint, rightPoint *tools.Coordinates) {
-	angleLeftSidePoint := (math.Pi / 2) + b.Angle
-	angleRightSidePoint := (math.Pi / 2) + math.Pi + b.Angle
+	angleLeftSidePoint := (math.Pi / 2) + b.TravelAngle
+	angleRightSidePoint := (math.Pi / 2) + math.Pi + b.TravelAngle
 	return trig.FindPointFromPoint(b.Origin, angleLeftSidePoint, b.Width/2), trig.FindPointFromPoint(b.Origin, angleRightSidePoint, b.Width/2)
 }
 
@@ -86,7 +95,13 @@ func Generator(boidsNumber int16) []Boid {
 		angle := float64(rand.Intn(360))
 		angleRad := angle * (math.Pi / 180)
 
-		boids[i] = New(float64(x), float64(y), angleRad)
+		var travelAngleDirection bool
+		if int(angle)%2 == 1 {
+			travelAngleDirection = true
+		} else {
+			travelAngleDirection = false
+		}
+		boids[i] = New(float64(x), float64(y), angleRad, travelAngleDirection)
 	}
 	return boids
 }
--- internal/boids/movements.go
+++ internal/boids/movements.go
@@ -9,16 +9,18 @@ import (
 )
 
 func (b *Boid) MoveForward() {
+	b.TravelRotation()
+
 	isColliding, collidingPositon := b.IsCollidingBorder()
 	if isColliding {
 		b.Rotate(collidingPositon)
 	}
 
-	newVertexCoordinates := trig.FindPointFromPoint(b.Vertex, b.Angle, float64(b.Speed)*tools.DeltaTime)
-	newOriginCoordinates := trig.FindPointFromPoint(b.Origin, b.Angle, float64(b.Speed)*tools.DeltaTime)
-
-	b.Vertex = newVertexCoordinates
+	newOriginCoordinates := trig.FindPointFromPoint(b.Origin, b.TravelAngle, float64(b.Speed)*tools.DeltaTime)
 	b.Origin = newOriginCoordinates
+
+	newVertexCoordinates := trig.FindPointFromPoint(b.Origin, b.TravelAngle, b.Height)
+	b.Vertex = newVertexCoordinates
 
 	b.drawBoidBody()
 }
@@ -54,9 +56,34 @@ func (b *Boid) Rotate(collidingPositon []int8) {
 		b.Angle = b.Angle - math.Pi*2
 	}
 
+	b.TravelAngle = b.Angle
+
 	// MAY BE REFACTOR THIS PART FOR SMOOTHER ROTATION
 	b.Origin = b.Vertex
 	b.Vertex = trig.FindPointFromPoint(b.Origin, b.Angle, b.Height)
+}
+
+func (b *Boid) TravelRotation() {
+	if b.TravelAngleDirection == true && b.TravelAngle >= b.MaxTravelAngle {
+		b.TravelAngleDirection = false
+		b.MaxTravelAngle = b.Angle - float64(b.MaxTravelAngleValue)*math.Pi/180
+	} else if b.TravelAngleDirection == false && b.TravelAngle <= b.MaxTravelAngle {
+		b.TravelAngleDirection = true
+		b.MaxTravelAngle = b.Angle + float64(b.MaxTravelAngleValue)*math.Pi/180
+	}
+
+	if b.MaxTravelAngle > math.Pi*2 {
+		b.TravelAngle = b.MaxTravelAngle - math.Pi*2
+	}
+	if b.MaxTravelAngle < 0 {
+		b.Angle = b.MaxTravelAngle + math.Pi*2
+	}
+
+	if b.TravelAngleDirection == true {
+		b.TravelAngle = b.TravelAngle + tools.DeltaTime*b.AngleSpeed
+	} else if b.TravelAngleDirection == false {
+		b.TravelAngle = b.TravelAngle - tools.DeltaTime*b.AngleSpeed
+	}
 }
 
 func (b *Boid) IsCollidingBorder() (bool, []int8) {
